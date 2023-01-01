2a5296d100bc31567cf0f1d4f38dc115d90c0062
CHUNTING, LIN <120126519+LINDaniel1008@users.noreply.github.com>
2023-01-01 11:33:58 -0700

    Create OverrideDemo.java

---
+++ OverrideDemo.java
@@ -0,0 +1,16 @@
+public class OverrideDemo 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		SubClass Obj1 = new SubClass();
+		Obj1.display("Hi");
+		
+		SubClass Obj2 = new SubClass();
+		Obj2.display("Hello again...");
+		
+	}
+
+}
